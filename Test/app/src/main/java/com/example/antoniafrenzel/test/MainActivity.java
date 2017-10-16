96b3d4f975c19805daf8a2f849b5bb4f0c9a292d
mailantonia <32844423+mailantonia@users.noreply.github.com>
2017-10-16 22:26:05 +0200

    new content main

--- Test/app/src/main/java/com/example/antoniafrenzel/test/MainActivity.java
+++ Test/app/src/main/java/com/example/antoniafrenzel/test/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        
+
     }
 
     @Override
