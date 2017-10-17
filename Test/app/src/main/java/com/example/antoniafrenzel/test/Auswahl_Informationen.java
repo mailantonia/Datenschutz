526f053bc3ace0972bad19c2c7a173182c61c533
ClaraLa <32844976+ClaraLa@users.noreply.github.com>
2017-10-17 12:31:16 +0200

    Version 17.10.

--- Test/app/src/main/java/com/example/antoniafrenzel/test/Auswahl_Informationen.java
+++ Test/app/src/main/java/com/example/antoniafrenzel/test/Auswahl_Informationen.java
@@ -10,5 +10,13 @@ public class Auswahl_Informationen {
                 "Jeder der das Internet nutzt hinterlässt Spuren über seine Identität und/oder über sein Nutzerverhalten.",
                 "Internetnutzer gehen häufig unachtsam mit ihren persönlichen Daten im Internet um –  aufgrund von blindem Vertrauen in Anbieter und dem Gefühl von Anonymität im Internet.",
                 "Durch das „Internet of Things“ wird das Thema Datenschutz immer präsenter, da technische Geräte (beispielsweise Kühlschränke oder Alarmanlagen) über den Anschluss zum Internet persönliche Daten über das Nutzerverhalten weitergeben.",
+                "Studien zeigen, dass Deutsche im Vergleich zu anderen Nationalitäten ein eher ausgeprägtes Bewusstsein zum Datenschutz haben.",
+                "Fazit: Es ist wichtig das Recht auf Privatsphäre jedes Nutzers im Internet zu wahren. Zudem müssen Nutzer über die Weitergabe und das Sammeln ihrer persönlichen Daten ausreichend aufgeklärt werden. Ein Bewusstsein hierfür gilt es zu Schaffen.",
         };
+
+        public String getInfo(int i)
+        {
+                String Wissen = Infos[i];
+                return Wissen;
+        }
 }
--- Test/app/src/main/java/com/example/antoniafrenzel/test/Informationen.java
+++ Test/app/src/main/java/com/example/antoniafrenzel/test/Informationen.java
@@ -1,5 +1,7 @@
 package com.example.antoniafrenzel.test;
 
+import android.graphics.ImageFormat;
+import android.icu.text.IDNA;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -7,18 +9,33 @@ import android.widget.TextView;
 
 public class Informationen extends AppCompatActivity {
 
-    private Auswahl_Informationen Infos = new Auswahl_Informationen();
+    private Auswahl_Informationen N_Infos = new Auswahl_Informationen();
 
-    private TextView textView3;
-    private Button button7;
+    private TextView N_textView3;
+    private Button But_Weiter;
+    int InfoArrayNumber = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informationen);
+
+        //Variablen zuweisen aus Designer
+        N_textView3 = (TextView) findViewById(R.id.textView3);
+        But_Weiter = (Button) findViewById(R.id.button7);
+
+        //Funktion aufrufen, um Textfeld aus Array zu füllen
+        updatedInfos();
     }
 
+    public void Button_Click()
+    {
+        InfoArrayNumber++;
+        updatedInfos();
+    }
 
+    private void updatedInfos()
+    {
+        N_textView3.setText(N_Infos.getInfo(InfoArrayNumber));
+    }
 }
-
-
