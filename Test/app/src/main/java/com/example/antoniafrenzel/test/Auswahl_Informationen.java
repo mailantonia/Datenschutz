67c2f9b3e3b5f8b86bbd9b772ce681a96d038ac1
ClaraLa <32844976+ClaraLa@users.noreply.github.com>
2017-10-27 14:16:01 +0200

    neu

--- Test/app/src/main/java/com/example/antoniafrenzel/test/Auswahl_Informationen.java
+++ Test/app/src/main/java/com/example/antoniafrenzel/test/Auswahl_Informationen.java
@@ -6,10 +6,10 @@ package com.example.antoniafrenzel.test;
 
 public class Auswahl_Informationen {
         public String Infos[]= {
-                "„Sammelbegriff über die in verschiedenen Gesetze zum Schutz des Individuums angeordneten Rechtsnormen, die erreichen sollen, dass seine Privatsphäre in einer zunehmend automatisierten und computerisierten Welt vor unberechtigten Zugriffen von außen geschützt wird.“\n" + "Quelle: Wirtschaftslexikon Gabler\n" + "\n",
-                "Jeder der das Internet nutzt hinterlässt Spuren über seine Identität und sein Nutzerverhalten.",
-                "Daher sollte jeder Internetnutzer beachten:\n\n(1.) Selbstschutz: Achtsam mit persönlichen Daten umzugehen.\n\n(2.) Fremdschutz: Den Nutzer durch Gesetze zu schützen.",
-                "Internetnutzer gehen häufig unachtsam mit ihren persönlichen Daten im Internet um –  aufgrund von blindem Vertrauen in Anbieter und dem Gefühl von Anonymität im Internet.",
+                "„Sammelbegriff über die in verschiedenen Gesetze zum Schutz des Individuums angeordneten Rechtsnormen, die erreichen sollen, dass seine Privatsphäre in einer zunehmend automatisierten und computerisierten Welt vor unberechtigten Zugriffen von außen geschützt wird.\n\nQuelle: Wirtschaftslexikon Gabler\n" + "\n",
+                "Jeder, der das Internet nutzt hinterlässt Spuren über seine Identität und sein Nutzerverhalten.",
+                "Daher sollte jeder Internetnutzer beachten:\n\n(1) Selbstschutz: Achtsam mit persönlichen Daten umzugehen.\n\n(2) Fremdschutz: Den Nutzer durch Gesetze zu schützen.",
+                "Internetnutzer gehen häufig unachtsam mit ihren persönlichen Daten um –  aufgrund von blindem Vertrauen in Anbieter und dem Gefühl von Anonymität im Internet.",
                 "Durch das „Internet of Things“ wird das Thema Datenschutz immer präsenter, da technische Geräte (beispielsweise Kühlschränke oder Alarmanlagen) über den Anschluss zum Internet persönliche Daten über das Nutzerverhalten weitergeben.",
                 "Studien zeigen, dass Deutsche im Vergleich zu anderen Nationalitäten ein eher ausgeprägtes Bewusstsein zum Datenschutz haben.",
                 "Fazit: Es ist wichtig das Recht auf Privatsphäre jedes Nutzers im Internet zu wahren. Zudem müssen Nutzer über die Weitergabe und das Sammeln ihrer persönlichen Daten ausreichend aufgeklärt werden. Ein Bewusstsein hierfür gilt es zu Schaffen.",
--- Test/app/src/main/java/com/example/antoniafrenzel/test/Check_button_passwort.java
+++ Test/app/src/main/java/com/example/antoniafrenzel/test/Check_button_passwort.java
@@ -26,7 +26,7 @@ public class Check_button_passwort extends AppCompatActivity {
         Teaser.setText("Häufig vergeben Internetnutzer spontan ein Passwort, dass sie sich leicht merken können oder bereits mehrfach als Passwort zu anderen Accounts nutzen. Das kann zu Sicherheitslücken führen. Wie sicher ist Dein Passwort?");
 
         Informationstext = (TextView) findViewById(R.id.textView5);
-        Informationstext.setText("\n1. Ein sicheres Passwort besteht aus Klein- und    Großbuchstaben sowie Zahlen und Sonderzeichen.\n\n2. Verwende kein geläufiges Wort sondern eine Abfolge von    Buchstaben, die für Dritte keinen Sinn ergeben.\n\n3. Tipp: Merke Dir beispielsweise einen Satz und verwende    die Anfangsbuchstaben der Wörter als Passwort. \n\nTeste die Sicherheit Deines persönlichen Passwortes unter folgendem Link:\n");
+        Informationstext.setText("\n1. Ein sicheres Passwort besteht aus Klein- und\n    Großbuchstaben sowie Zahlen und Sonderzeichen.\n\n2. Verwende kein geläufiges Wort sondern eine Abfolge\n     von Buchstaben, die für Dritte keinen Sinn ergibt.\n\n3. Tipp: Merke Dir beispielsweise einen Satz und\n     verwende die Anfangsbuchstaben der Wörter als\n     Passwort. \n\nTeste die Sicherheit Deines persönlichen Passwortes unter folgendem Link:\n");
 
         Link = (TextView) findViewById(R.id.textView7);
         Link.setText("https://checkdeinpasswort.de/");
