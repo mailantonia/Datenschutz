af50e80878403e67b7442f32d7a80493fcc73477
mailantonia <32844423+mailantonia@users.noreply.github.com>
2017-10-27 14:24:23 +0200

    Impressum

--- Test/app/src/main/java/com/example/antoniafrenzel/test/Impressum.java
+++ Test/app/src/main/java/com/example/antoniafrenzel/test/Impressum.java
@@ -1,7 +1,9 @@
 package com.example.antoniafrenzel.test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Impressum extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class Impressum extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_impressum);
+    }
+
+    public void startNewActivity(View v) {
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+
     }
 }
